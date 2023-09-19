c6032304c4ea6fa8a8aa2e911e37f28b2ff9bc7c
赵宏宇 <8@163.com>
2023-09-19 11:26:34 +0800

    开源聊天扩展包初始项目

--- src/enum/MsgEnum.php
+++ src/enum/MsgEnum.php
@@ -12,9 +12,12 @@ abstract class MsgEnum
     // 系统通知
     const SYSTEM = 'system';
     /**
-     *
+     * 系统通知-好友申请
      */
     const SYSTEM_FRIEND_APPLY = 'system_friend_apply';
-//    const SYSTEM = 'system';
+    /**
+     * 系统通知-站内消息
+     */
+    const SYSTEM_PRIVATE_MSG = 'system_private_msg';
 //    const SYSTEM = 'system';
 }
