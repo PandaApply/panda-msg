cf2a4838c7ab4d11197cc8bd9df43fb256639c90
赵宏宇 <8@163.com>
2023-09-19 09:47:09 +0800

    开源聊天扩展包初始项目

---
+++ src/enum/MsgEnum.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace panda\msg\enum;
+
+/**
+ * User: ZhaoHongYu
+ * Date: 2023/9/19
+ * Desc: 消息类型
+ */
+abstract class MsgEnum
+{
+    // 系统通知
+    const SYSTEM = 'system';
+    /**
+     *
+     */
+    const SYSTEM_FRIEND_APPLY = 'system_friend_apply';
+//    const SYSTEM = 'system';
+//    const SYSTEM = 'system';
+}
