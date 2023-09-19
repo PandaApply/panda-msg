603ccd0b1008b37e474284ab6b93d325de0541c7
赵宏宇 <8@163.com>
2023-09-19 09:05:29 +0800

    开源聊天扩展包初始项目

--- src/facade/PandaMsg.php
+++ src/facade/PandaMsg.php
@@ -9,9 +9,9 @@ use think\Facade;
  * Date: 2023/9/15
  * Desc: 门面
  * @mixin \panda\msg\PandaMsg
- * @method void sendAll() 向所有客户端连接或者client_id_array指定的客户端连接广播消息
- * @method bool sendMsgGroup() 向 group 发送
- * @method void sendMsgUid() 向uid绑定的所有在线client_id发送数据。
+ * @method void sendAll(array $data, array $client_id_array = null, array $exclude_client_id = null, bool $raw = false) 向所有客户端连接或者client_id_array指定的客户端连接广播消息
+ * @method bool sendMsgGroup(array|int|string $group, array $data, array $exclude_client_id = null, bool $raw = false) 向 group 发送
+ * @method void sendMsgUid(int|string|array $uid, array $data) 向uid绑定的所有在线client_id发送数据。
  */
 class PandaMsg extends Facade
 {
